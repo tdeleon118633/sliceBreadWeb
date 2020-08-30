5a1d715cb8ae205787a9adf27ca7ae2df9b3b6a1
tdeleon <compuaserv@gmail.com>
2020-08-29 22:54:02 -0600

    Alarmas, INSERTAR/ACTUALIZAR/BORRAR

--- config/Conexion.php
+++ config/Conexion.php
@@ -15,9 +15,32 @@
 									<button type="button" class="close" data-dismiss="alert" aria-label="Close">
 										<span aria-hidden="true">&times;</span>
 									</button>
-									<strong>Exitos!! </strong>Registro guardado
+									<strong>Exitos!! </strong>El Registro fue Insertado Satifactoriamente al sistema.
 								</div>
 							</center>';
+		}
+
+		public function fntBorrar(){
+
+			echo '<center><div class="alert alert-danger alert-dismissible fade in" role="alert">
+					 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+						 <span aria-hidden="true">&times;</span>
+					 </button>
+					 <strong>Exitos!! </strong>El Registro fue Borrado Satifactoriamente al sistema.
+				 </div>
+			 </center>';
+
+		}
+
+		public function fntEdicion(){
+
+			echo '<center><div class="alert alert-success alert-dismissible fade in" role="alert">
+					 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+						 <span aria-hidden="true">&times;</span>
+					 </button>
+					 <strong>Exitos!! </strong>El Registro fue Editado Satifactoriamente al sistema.
+				 </div>
+			 </center>';
 		}
 	}
 ?>
